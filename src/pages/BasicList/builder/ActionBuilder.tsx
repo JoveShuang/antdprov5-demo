14c6537f524f93693085da8db0dcde83dcb7fa67
Demetrius Liu <shuangstar@icloud.com>
2021-10-06 10:45:01 +0800

    feat(antdpro): 新增提交表单逻辑

--- src/pages/BasicList/builder/ActionBuilder.tsx
+++ src/pages/BasicList/builder/ActionBuilder.tsx
@@ -1,11 +1,20 @@
 import { Button } from 'antd';
 import type { ButtonType } from 'antd/lib/button';
 
-const ActionsBuilder = (actions: BasicListApi.Action[] | undefined) => {
+const ActionsBuilder = (
+  actions: BasicListApi.Action[] | undefined,
+  actionHandler: BasicListApi.Action,
+) => {
   return (actions || []).map((action: any) => {
     if (action.component === 'button') {
       return (
-        <Button key={action.text} type={action.type as ButtonType}>
+        <Button
+          key={action.text}
+          type={action.type as ButtonType}
+          onClick={() => {
+            actionHandler(action);
+          }}
+        >
           {action.text}
         </Button>
       );
--- src/pages/BasicList/component/Modal.tsx
+++ src/pages/BasicList/component/Modal.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { useRequest } from 'umi';
-import { Modal as AntdModal, Form } from 'antd';
+import { Modal as AntdModal, Form, Input } from 'antd';
+import moment from 'moment';
 import FormBuilder from '../builder/FormBuilder';
 import ActionsBuilder from '../builder/ActionBuilder';
 
@@ -13,27 +14,109 @@ const Modal = ({
   hideModal: () => void;
   modalUri: string;
 }) => {
-  const init = useRequest<{ data: PageApi.Data }>(`${modalUri}`);
+  const [form] = Form.useForm();
+  const init = useRequest<{ data: PageApi.Data }>(`${modalUri}`, {
+    manual: true,
+  });
+  const request = useRequest(
+    ({ uri, method, ...formValues }) => {
+      return {
+        url: `https://public-api-v2.aspirantzhang.com${uri}`,
+        method,
+        data: {
+          ...formValues,
+          'X-API-KEY': 'antd',
+          create_time: moment(formValues.create_time).format(),
+          update_time: moment(formValues.update_time).format(),
+        },
+      };
+    },
+    {
+      manual: true,
+    },
+  );
 
   useEffect(() => {
     if (modalVisible) {
+      form.resetFields();
       init.run();
     }
   }, [modalVisible]);
+
+  const setFieldsAdaptor = (data: PageApi.Data) => {
+    if (data?.layout?.tabs && data?.dataSource) {
+      const result = {};
+      data.layout.tabs.forEach((tab: any) => {
+        tab.data.forEach((field: any) => {
+          switch (field.type) {
+            case 'datetime':
+              result[field.key] = moment(data.dataSource[field.key]);
+              break;
+            default:
+              result[field.key] = data.dataSource[field.key];
+              break;
+          }
+        });
+      });
+      return result;
+    }
+    return {};
+  };
+
+  useEffect(() => {
+    if (init.data) {
+      form.setFieldsValue(setFieldsAdaptor(init?.data));
+    }
+  }, [init.data]);
+
   const layout = {
-    laeblCol: { span: 8 },
+    laeblcol: { span: 8 },
     wrapperCol: { span: 16 },
   };
+
+  const onFinish = (values: any) => {
+    request.run(values);
+  };
+
+  const actionHandler = (action: BasicListApi.Action) => {
+    // const { action, uri, method } = action
+    switch (action.action) {
+      case 'submit':
+        form.setFieldsValue({ uri: action.uri, method: action.method });
+        form.submit();
+        break;
+      default:
+        break;
+    }
+  };
+
   return (
     <div>
       <AntdModal
         title={init?.data?.page?.title}
         visible={modalVisible}
-        // onOk={}
         onCancel={hideModal}
-        footer={ActionsBuilder(init?.data?.layout?.actions[0]?.data)}
+        footer={ActionsBuilder(init?.data?.layout?.actions[0]?.data, actionHandler)}
+        maskClosable={false}
       >
-        <Form {...layout}>{FormBuilder(init?.data?.layout?.tabs[0]?.data)}</Form>
+        <Form
+          form={form}
+          {...layout}
+          initialValues={{
+            create_time: moment(),
+            update_time: moment(),
+            status: true,
+          }}
+          onFinish={onFinish}
+        >
+          {FormBuilder(init?.data?.layout?.tabs[0]?.data)}
+          <Form.Item name="uri" key="uri" hidden>
+            <Input />
+          </Form.Item>
+          <Form.Item name="method" key="method" hidden>
+            <Input />
+          </Form.Item>
+        </Form>
       </AntdModal>
     </div>
   );
